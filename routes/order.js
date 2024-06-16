b0742368f8b353561337491255543b2361a9aaae
Allison <theallisonlai@fakemail.com>
2024-06-16 23:30:01 +0800

    feat: 新增兩隻api(買方查詢所有訂單紀錄、查詢指定訂單詳情)

--- routes/order.js
+++ routes/order.js
@@ -6,4 +6,10 @@ const orderController = require('../controllers/user/orderControllers');
 //單一訂單管理
 router.post('/', isAuth, orderController.createOrder);
 
+
+// 查询買家所有訂單紀錄
+router.get('/', isAuth, orderController.getOrders);
 module.exports = router;
+
+// 查詢單張訂單詳情
+router.get('/:orderId', isAuth, orderController.getSpecificOrder);
